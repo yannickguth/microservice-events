90816e43ea646e626ab2a19084cc5a5af772b507
ADESSO\guth <yannick.guth@adesso.de>
2017-12-05 10:02:13 +0100

    eureka support

--- src/main/java/de/cluster/microservices/events/MicroserviceEventsApplication.java
+++ src/main/java/de/cluster/microservices/events/MicroserviceEventsApplication.java
@@ -2,8 +2,10 @@ package de.cluster.microservices.events;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroserviceEventsApplication {
 
     public static void main(String[] args) {
